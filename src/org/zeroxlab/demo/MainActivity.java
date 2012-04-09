6f25c461b581df1af7cde575a37519fd3c22fef2
Julian_Chu <walkingice@0xlab.org>
2012-04-09 10:42:33 +0800

    Init Project
    $ android create project -p . -t 5 -k org.zeroxlab.demo -n
    SlidingSidebar -a MainActivity

---
+++ src/org/zeroxlab/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package org.zeroxlab.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
